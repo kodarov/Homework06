3038d52d6bd28a6a8555ef7056786964de19f181
kodarov_salavat <kodarov@gmail.com>
2023-01-09 20:34:55 +0400

    hw06\task06

--- src/Main.java
+++ src/Main.java
@@ -52,6 +52,9 @@ public class Main {
 
     public static void task6() {
         System.out.println("Задача 6");
+        for (int i = 7; i <= 98; i += 7) {
+            System.out.println(i);
+        }
     }
 
     public static void task7() {
