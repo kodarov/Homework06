fd093105ceeb6dd49e1fcd3afa005fae6da1b0d2
kodarov_salavat <kodarov@gmail.com>
2023-01-09 20:04:08 +0400

    hw06\task03

--- src/Main.java
+++ src/Main.java
@@ -11,40 +11,55 @@ public class Main {
         task9();
         task10();
     }
+
     public static void task1() {
         System.out.println("Задача 1");
         //решение
-        for (int i=1;i<11;i++) {
+        for (int i = 1; i < 11; i++) {
             System.out.println(i);
         }
     }
+
     public static void task2() {
         System.out.println("Задача 2");
-        for (int i=10;i>0;i--) {
+        for (int i = 10; i > 0; i--) {
             System.out.println(i);
         }
     }
+
     public static void task3() {
         System.out.println("Задача 3");
+        for (int i = 1; i < 18; i++) {
+            if ((i % 2) == 0) {
+                System.out.println(i);
+            }
+        }
     }
+
     public static void task4() {
         System.out.println("Задача 4");
     }
+
     public static void task5() {
         System.out.println("Задача 5");
     }
+
     public static void task6() {
         System.out.println("Задача 6");
     }
+
     public static void task7() {
         System.out.println("Задача 7");
     }
+
     public static void task8() {
         System.out.println("Задача 8");
     }
+
     public static void task9() {
         System.out.println("Задача 9");
     }
+
     public static void task10() {
         System.out.println("Задача 10");
     }
